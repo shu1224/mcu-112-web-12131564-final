2359451c9c1b45ef960379241507d298a2cb0267
shu <syu220922@gmail.com>
2024-01-15 16:25:38 +0800

    feat(article): 文章預覽元件與清單實作

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { TagListComponent } from '../tag-list/tag-list.component';
+import { ArticleListComponent } from '../article-list/article-list.component';
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [TagListComponent],
+  imports: [ArticleListComponent, TagListComponent],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css',
 })
