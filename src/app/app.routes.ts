82530ce38d6e2b94968b98192e79cd7e8f9f79fc
shu <syu220922@gmail.com>
2024-01-15 21:28:42 +0800

    feat(article-form): 新文章表單頁面切換實作

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 
+import { ArticleFormPageComponent } from './article-form-page/article-form-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { SingInPageComponent } from './sing-in-page/sing-in-page.component';
 import { SingUpPageComponent } from './sing-up-page/sing-up-page.component';
@@ -9,4 +10,5 @@ export const routes: Routes = [
   { path: 'home', component: HomePageComponent },
   { path: 'sign-in', component: SingInPageComponent },
   { path: 'sign-up', component: SingUpPageComponent },
+  { path: 'article/form', component: ArticleFormPageComponent },
 ];
